3781def0b47a695a76131aa31d371d34efe76c71
rafalroman <rafalroman@gmail.com>
2015-03-13 17:58:42 +0100

    initial commit

---
+++ nodetest.js
@@ -0,0 +1,7 @@
+var nodePath = "c:/Program Files/nodejs";
+var moment = require(nodePath + "/node_modules/moment/moment.js");
+
+
+console.log("test message", moment().format());
+var mojaZmienna = "Rafa";
+console.log(mojaZmienna);
